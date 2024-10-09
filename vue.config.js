e33acbf27e9b13f9032bdde20bec26ff50de32b9
Abdalla34 <abogabla88@gmail.com>
2024-10-09 17:23:40 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,13 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  // proxy: {
-  //   "/api": {
-  //     target: "https://gabal-ecommerce-api.vercel.app", // API server URL
-  //     changeOrigin: true, // Changes the origin of the host header to the target URL
-  //     pathRewrite: { "^/api": "/api" }, // Rewrites the URL path
-  //   },
-  // },
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "https://gabal-ecommerce-api.vercel.app", // API server URL
+        changeOrigin: true, // Changes the origin of the host header to the target URL
+        pathRewrite: { "^/api": "/api" }, // Rewrites the URL path
+      },
+    },
+  },
 });
